de3039054bbfb079c0655b0e2a6f5f21e416127f
LG <LG@DESKTOP-PQF3C5V>
2019-07-21 18:46:13 +0900

    update calendar (korean -> english)

--- src/jihye_calendar/calendar.java
+++ src/jihye_calendar/calendar.java
@@ -3,7 +3,7 @@ package jihye_calendar;
 public class calendar {
 
 	public static void main(String[] args) {
-		System.out.println(" 일   월   화   수   목   금   토");
+		System.out.println(" S  M  T  W  T  F  S");
 		System.out.println("----------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
 		System.out.println(" 8  9 10 11 12 13 14");
