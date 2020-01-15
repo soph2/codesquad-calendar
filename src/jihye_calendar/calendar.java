92ccbd3681dc8837d2cd2cbbe2e8e51be4afa80a
soph2 <gdqd2e2e@gmail.com>
2020-01-15 14:44:18 +0900

    add the week

--- src/jihye_calendar/calendar.java
+++ src/jihye_calendar/calendar.java
@@ -9,6 +9,7 @@ public class calendar {
 		System.out.println(" 8  9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+		System.out.println("29 30");
 	}
 
 }
