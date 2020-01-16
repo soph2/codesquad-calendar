2520b8797bbcab1f2a5dde89b1ba6e71481d0c1b
soph2 <gdqd2e2e@gmail.com>
2020-01-16 16:54:45 +0900

    Add repeat function using for

--- src/jihye_calendar/calendar.java
+++ src/jihye_calendar/calendar.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class calendar {
 
-	private static final int[] MAX_DAYS= {31,28,31,30,31,30,31,31,30,31,30,31};
-	
+	private static final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int maxDaysOfMonth(int month) {
-		return MAX_DAYS[month-1];
+		return MAX_DAYS[month - 1];
 	}
-	
+
 	public int getMaxDaysOfMonth(int month) {
 		switch (month) {
 		case 2:
@@ -22,9 +22,9 @@ public class calendar {
 			return 31;
 		}
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println(" S  M  T  W  T  F  S");
 		System.out.println("----------------------");
 		System.out.println(" 1  2  3  4  5  6  7");
@@ -32,12 +32,22 @@ public class calendar {
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
 		System.out.println("29 30");
-		
+
 		Scanner scan = new Scanner(System.in);
-		int month = scan.nextInt();
-		calendar cal=new calendar();
-		System.out.printf("%d월의 최대 일수는 %d일\n",month,cal.maxDaysOfMonth(month));
+		Scanner scancnt = new Scanner(System.in);
+		System.out.println("How many do you want to know the max day of month?");
+		int cnt = scancnt.nextInt();
+		for (int i = 0; i < cnt; i++) {
+			System.out.println("Input the month");
+			int month = scan.nextInt();
+			calendar cal = new calendar();
+			System.out.printf("%d월의 최대 일수는 %d일\n", month, cal.maxDaysOfMonth(month));
+		}
+		System.out.println("End");
 		scan.close();
+		scancnt.close();
+
 	}
 
 }
+//arrange : Ctrl + Shift + F
